b9692a08daaf326e11e4a49b1dc2c1c44b41e709
Thomas Johnson-Ellis <tomjohnsonellis@protonmail.com>
2023-01-10 01:36:04 +0000

    Upload first api calls
    Also update .gitignore

--- course content/oanda_firststeps.py
+++ course content/oanda_firststeps.py
@@ -16,3 +16,27 @@ print(api.get_instruments())
 instruments = api.get_instruments()
 
 print("There are {} different instruments, E.g. {}".format(len(instruments), instruments[0]))
+
+#####
+# We can get historical price data (which we will be using to train models) like so:
+help(api.get_history)
+df = api.get_history(
+    instrument="EUR_USD",  # The instrument we care about
+    start="2020-07-01",  # Start date for data
+    end="2020-07-31",  # End data for data
+    granularity="D",  # Time-frame, like Day(D), 30 minutes(M30)...
+    price="B"  # (B)id or (A)sk price
+)
+
+print("df column names: {}".format(df.columns))
+
+#####
+# Loading live data isn't too difficult!
+# I imagine that actually using it will be...
+
+# We can start "listening" to the stream of data like so:
+api.stream_data("EUR_USD", stop=5)
+# If we didn't have a timeout, we can stop like so:
+api.stop_stream
+
+
